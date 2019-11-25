1ace0ed527ecc0e08e5fdd74a57b413a85079047
lauren maxwell <laurenelizamax@icloud.com>
2019-11-25 11:53:09 -0600

    added some methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace interfacePractices
 {
@@ -6,7 +7,34 @@ namespace interfacePractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<ISwimming> swimmers = new List<ISwimming>();
+            List<IWalking> walkers = new List<IWalking>();
+
+            // Instance of a tesla class, we treat it as an ielectricvehicle
+            var myDog = new PaintedDog();
+            var myTurtle = new SeaTurtle();
+
+
+            swimmers.Add(myTurtle);
+
+            walkers.Add(myDog);
+
+            foreach (var animal in swimmers)
+            {
+                animal.Swim();
+                {
+                    Console.WriteLine("He's a fast swimmer");
+                }
+            }
+
+            foreach (var animal in walkers)
+            {
+                animal.Run();
+                {
+                    Console.WriteLine("He's a slow runner");
+
+                }
+            }
         }
     }
 }
